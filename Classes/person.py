f987a64463dc9b2599c96d32369bd313c3fed86b
Дмитрий Орлов <dotaonelove332@gmail.com>
2023-10-15 18:04:34 +0300

    +

--- Classes/person.py
+++ Classes/person.py
@@ -1,11 +1,19 @@
 class Person:
+    # Создаем метод init для инициализации объекта при создании
     def __init__(self, name, surname, qualification=1):
+        # Сохраняем значение атрибута name в переменную self.name
         self.name = name
+        # Сохраняем значение атрибута surname в переменную self.surname
         self.surname = surname
+        # Сохраняем значение атрибута qualification в переменную self.qualification, если оно не было передано при создании объекта, то по умолчанию равно 1
         self.qualification = qualification
     
+    # Создаем метод get_info для получения информации об объекте
     def get_info(self):
+        # Возвращаем строку с информацией об объекте, используя значения атрибутов self.name, self.surname и self.qualification
         return f"Имя: {self.name}\nФамилия: {self.surname}\nКвалификация: {self.qualification}"
     
+    # Создаем метод del для прощания с объектом
     def __del__(self):
+        # Выводим сообщение о прощании с объектом, используя значения атрибутов self.name и self.surname
         print(f"До свидания, мистер {self.name} {self.surname}")
--- Lab1Sort/sort.py
+++ Lab1Sort/sort.py
@@ -1,33 +1,54 @@
 import random
 
+# Создаем функцию для сортировки пузырьком
 def bubble_sort(arr):
+    # Цикл for перебирает все элементы списка
     for i in range(len(arr)):
+        # Вложенный цикл for перебирает все элементы списка, кроме последнего
         for j in range(len(arr) - 1):
+            # Если текущий элемент больше следующего, то меняем их местами
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+# Создаем функцию для генерации случайного списка
 def random_array(a,b,c):
+    # Создаем пустой список
     arr = []
+    # Цикл for добавляет в список a случайных чисел в диапазоне от b до c
     for i in range(a):
         arr.append(random.randint(b, c))
     return arr
 
+# Создаем функцию для чтения данных из файла
 def read_file(a):
+    # Открываем файл с именем a и режимом чтения
     file = open( a + ".txt", "r")
+    # Считываем данные из файла и сохраняем их в переменную data
     data = file.read()
+    # Закрываем файл
     file.close()
+    # Преобразуем данные из строки в список
     array = eval(data)
+    # Вызываем функцию convert_to_list для преобразования списка в список списков
     arr = convert_to_list(array)
     return arr
 
+# Создаем функцию для записи данных в файл
 def write_file(a, arr):
+    # Открываем файл с именем a и режимом записи
     file = open(a + ".txt", "w")
+    # Преобразуем список в строку
     arr_str = str(arr)
+    # Записываем строку в файл
     file.write(arr_str)
+    # Закрываем файл
     file.close()
 
+# Создаем функцию для преобразования списка в список списков
 def convert_to_list(arr):
+    # Создаем пустой список
     lst = []
-    for elem in arr:
-        lst.append(elem)
+    # Цикл for добавляет каждый элемент из списка arr в список lst
+    for el in arr:
+        lst.append(el)
     return lst
